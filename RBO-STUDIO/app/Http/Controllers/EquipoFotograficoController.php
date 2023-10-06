d5dfbaadada2b86d2fded1777332b816713d00e6
Jesusbotellofavela <jesusbotellofavela@gmail.com>
2023-10-06 14:55:28 -0600

    creacion de controladores y modelos

---
+++ RBO-STUDIO/app/Http/Controllers/EquipoFotograficoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EquipoFotografico;
+use Illuminate\Http\Request;
+
+class EquipoFotograficoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EquipoFotografico $equipoFotografico)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EquipoFotografico $equipoFotografico)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, EquipoFotografico $equipoFotografico)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EquipoFotografico $equipoFotografico)
+    {
+        //
+    }
+}
